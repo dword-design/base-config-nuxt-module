f6f993aa45e51bbc84168a97d77bc3f98895c1d8
Sebastian Landwehr <info@sebastianlandwehr.com>
2024-04-08 16:43:55 +0000

    feat: init

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import nodeConfig from '@dword-design/base-config-node'
+import dedent from 'dedent'
+import loadPkg from 'load-pkg'
+
+export default config => {
+  const packageConfig = loadPkg.sync()
+
+  return {
+    ...nodeConfig(config),
+    readmeInstallString: dedent`
+      ## Install
+
+      \`\`\`bash
+      # npm
+      $ npx nuxi module add ${packageConfig.name}
+
+      # Yarn
+      $ yarn nuxi module add ${packageConfig.name}
+      \`\`\`
+    `,
+  }
+}

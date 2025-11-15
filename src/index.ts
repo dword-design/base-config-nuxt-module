1425ea77876be3736481310cce91ec94d51916e4
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-11-15 05:22:15 +0100

    fix: fix typescript error

--- src/index.ts
+++ src/index.ts
@@ -31,9 +31,14 @@ export default defineBaseConfig(function (
     eslintConfig: endent`
       import { createConfigForNuxt } from '${packageName`@nuxt/eslint-config`}/flat';
       import config from '@dword-design/eslint-config';
-      import { globalIgnores } from "eslint/config";
+      import { globalIgnores } from 'eslint/config';
+      import type { Linter } from 'eslint';
 
-      export default await createConfigForNuxt({ features: { standalone: false } })
+      /**
+       * TODO: Otherwise getting this error in the project using this package:
+       * error TS2742: The inferred type of 'default' cannot be named without a reference to '~/node_modules/@eslint/core/dist/cjs/types.cjs'. This is likely not portable. A type annotation is necessary.
+       **/
+      const result: Linter.Config[] = await createConfigForNuxt({ features: { standalone: false } })
         .prepend(
           config,
           {
@@ -43,7 +48,9 @@ export default defineBaseConfig(function (
           },
           globalIgnores(['eslint.config.ts', 'eslint.lint-staged.config.ts']),
         )
-        .toConfigs();\n
+        .toConfigs();
+
+      export default result;\n
     `,
     gitignore: [...nodeConfig.gitignore, '/.nuxt'],
     hasTypescriptConfigRootAlias: false,
